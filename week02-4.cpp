6ca91e4a5282e10ad5b00d1eb7da64af776a72c5
deverra <112918630+deverraa@users.noreply.github.com>
2022-09-13 12:01:39 +0800

    week02

---
+++ week02-4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("輸入兩個整數:");
+        scanf("%d",&a);
+        scanf("%d",&b);
+    printf("已收到%d , %d\n",a,b);
+    printf("他們加總是%d\n",a+b);
+}
