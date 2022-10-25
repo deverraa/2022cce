f037e7190844d7c6b48bee18e43ad6eb7fa20ec7
deverra <112918630+deverraa@users.noreply.github.com>
2022-10-25 11:32:32 +0800

    week08

---
+++ week08-1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int bad;
+    for(int i=2;i<n;i++)
+    {
+            if(n%i==0)bad=1;
+    }
+    if(bad=0)printf("%d是質數",n);
+    else printf("%d 不好,不是質數",n);
+
+}
