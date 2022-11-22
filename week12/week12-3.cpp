3442f4b1a132c3ca72b2536f921364beab41c042
deverraa <deverraa1110@gmail.com>
2022-11-22 11:55:40 +0800

    week12

---
+++ week12/week12-3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int a[10]={4,5,8,9,1,6,2,3,7};
+
+int main()
+{
+    int a[3]={7,8,9};
+    int b[2][3]={{4,5,6},{7,8,9}};
+
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+        printf("\n");
+    }
+}
